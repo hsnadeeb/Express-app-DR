5431030415bcf5d9898379c353f566a76e4bcfd2
Hasan Adeeb <hsnadeeb007@gmail.com>
2023-05-05 15:18:36 +0530

    seted up sequalize and started with it

--- app.js
+++ app.js
@@ -13,6 +13,7 @@ app.set('views', 'views');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const sequelize = require('./util/database');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,4 +23,11 @@ app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-app.listen(3000);
+sequelize.sync()
+.then(result=>{
+//    console.log(result);
+   app.listen(3000);
+})
+.catch(err=>{
+    console.log(err);
+});
